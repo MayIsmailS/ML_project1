ff1f6221a5499cb7cb9dea6e82eda525d9034e92
javiera-quiroz <72650941+javiera-quiroz@users.noreply.github.com>
2020-10-25 22:15:44 +0100

    Add files via upload

--- run_logistic_regression.py
+++ run_logistic_regression.py
@@ -1,15 +1,10 @@
-#! /usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 from helpers import *
 from ipywidgets import IntSlider, interact
-import math
 from plots import *
 from implementations import *
 from validation import *
-
-gammas_chosen_nll = {0:0.004281332398719396, 1:0.0014384498882876629, 2:0.01, 3:0.0014384498882876629}
 
 def main():
     print("start")
@@ -18,22 +13,31 @@ def main():
     y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
     DATA_TEST_PATH = 'test.csv'
     _, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
-
+    
     print("start predictions")
-    max_iters = 300
+    max_iters = 400
+    gammas_chosen_nll = {0: 0.004281332398719396, 
+                         1: 0.0014384498882876629, 
+                         2: 0.01, 
+                         3: 0.0014384498882876629
+                        }
+    # cross validation accuracies obtained after 200 iterations:
+    # 0.693950676595404, 0.6022516248839369, 0.4382642528188026, 0.6789839379173435
+    # 0.694, 0.602, 0.438, 0.679
     y_pred = []
+    
     for i in range(4):
         print("group ", i)
-
+        
         # prepare data
-        x_tr, _= clean_data(tX, i)
-        x_te, _ = clean_data(tX_test, i)
+        x_tr = clean_data(tX, i)
+        x_te = clean_data(tX_test, i)
         y_tr = y[tX[:, 22] == i]
-
+        
         x_tr, x_te = PCA_2(x_tr, x_te)
         y_tr, tx_tr = build_model_data(x_tr, y_tr)
-        _, tx_te = build_model_data(x_te, y_tr)
-
+        _, tx_te = build_model_data(x_te, x_te)
+        
         # initialize w
         d = tx_tr.shape[1]
         w = np.zeros(d)
@@ -42,10 +46,10 @@ def main():
         print("loss", loss)
         print(predict_labels(w, tx_te))
         y_pred.append(predict_labels(w, tx_te))
-
+    
     print("final step")
     predict(y_pred, tX_test, ids_test)
-    print("ended")
+    print("end")
 main()
 
 # 0.573 accuracy, 0.229 F-1 score
